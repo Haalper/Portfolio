154226f510c49f76d185346e33caa67c5f4e6f4e
Halisson Nascimento <halissonp@hotmail.com>
2024-03-07 23:08:28 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Haalper.png" />
-    <Titulo fontSize={20}>Halisson Nascimento</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Halisson Nascimento</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Haalper
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
